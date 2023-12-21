16bb2fcf3b1331c29990f4c4d3589e951002f154
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-12-20 23:46:39 -0500

    Added infinite scroll using react-intersection-observer

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,38 @@
+"use client";
+
+import { fetchAnime } from "@/app/actions";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+// react-intersection-observer
+
+let page = 2; // Declring teh page number here in a variable instead of a state is a much simpler solution
 
 function LoadMore() {
+  const { ref, inView } = useInView({});
+  const [data, setData] = useState<AnimeProp[]>([]); // An array of AnimeProps
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]), // spread the current data and add the new data from the response
+          page++; // increment the page number
+      });
+    }
+  }, [inView]);
+
   return (
     <>
+      {/* This section is for all teh subsequent pages from page 1 */}
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
